4b36c3a7c55f652a5cb38d4417e10b6c199dfcf0
Michael R Kost <michaelrkost@outlook.com>
2022-01-24 15:46:14 -0500

    Summer updates

--- ibUtils/buildCompanyFundamentalFromBarChart.py
+++ ibUtils/buildCompanyFundamentalFromBarChart.py
@@ -22,6 +22,172 @@ def buildExcelFile(aStock, startday, theExpiryDateText):
     stockOverview = companyInfo.getCompanyOverview(aStock)
     stockFundamentals = companyInfo.getCompanyFundamentals(aStock)
     aText, stockRatings = companyInfo.getCompanyRatings(aStock)
+    # callOptions, putOptions, expiryText, aWebDriver = companyOptions.scrapeCompanyOptionData(aStock, theExpiryDateText)
+
+    # Setup Excel output file
+    outExcelFile = theBaseCompaniesDirectory + startday + '/' + aStock + '_SummaryWeekOf-' + startday + excelSuffix
+    # Setup Fundamentals worksheet name
+    sheetIsFundamentals = aStock + '-Fundamentals'
+
+    # Starting point for data
+    sheetRowStart = 4
+    sheetColStart = 1
+
+    # create writer and workbook
+    writer = pd.ExcelWriter(outExcelFile, engine='xlsxwriter')
+    fundamentalsWorkbook = writer.book
+
+    # percentFormat  = fundamentalsWorkbook.add_format({'num_format': '0.0%'})
+    # currencyFormat = fundamentalsWorkbook.add_format({'num_format': '$#,##0.00'})
+
+    # ------------------------------------------------------------------
+    # set up fundamentals Worksheet
+    # ------------------------------------------------------------------
+    # Transpose
+    stockFundamentalsTranposed = stockFundamentals.T
+
+    # Add the Ratings next
+    stockRatings.to_excel(writer, sheet_name= sheetIsFundamentals,
+                                        startrow=sheetRowStart+3, startcol= sheetColStart)
+    # Add the fundamentals
+    stockInfo.to_excel(writer, sheet_name= sheetIsFundamentals,  startrow=sheetRowStart+7,
+                       startcol=sheetColStart, header=False)
+    stockFundamentalsTranposed.to_excel(writer, sheet_name= sheetIsFundamentals, startrow=sheetRowStart+7,
+                                        startcol= sheetColStart+2, header=False)
+    stockOverview.to_excel(writer, sheet_name= sheetIsFundamentals,
+                           startrow=sheetRowStart+7, startcol= sheetColStart+4, header=False)
+
+    # Add the text info on Expiry and Recommendations
+    fundamentalsWorkbookSheet = writer.sheets[sheetIsFundamentals]
+    fundamentalsWorkbookSheet.write(sheetRowStart+1,1, aText)
+    fundamentalsWorkbookSheet.write(sheetRowStart,1, 'expiry*********Text')
+    fundamentalsWorkbookSheet.set_column('B:F', 25)
+    fundamentalsWorkbookSheet.set_column('G:S', 18)
+    # fundamentalsWorkbookSheet.conditional_format('B2:B8', {'type': '3_color_scale'})
+
+    # ------------------------------------------------------------------
+    # setup the Call/Put Option Worksheets
+    # ------------------------------------------------------------------
+    # include the DF header as Excel Header
+    # callOptions.to_excel(writer, sheet_name= aStock + '-Call Options',
+    #                                     startrow=0, header=True)
+    # putOptions.to_excel(writer, sheet_name= aStock + '-Put Options',
+    #                                     startrow=0, header=True)
+
+    # ------------------------------------------------------------------
+    # setup the Call/Put Option Worksheets
+    # ------------------------------------------------------------------
+    # Get aStock Excel file and add as a sheet
+    inExcelFile = theBaseCompaniesDirectory + startday +\
+                          '/SummaryWeekOf-' + startday + excelSuffix
+    # # Need to use pd.ExcelFile to read in the file to manipulate
+    yahooEarningsDf_aSymbol_Sheet = pd.ExcelFile(inExcelFile).parse(aStock)
+    # # Add the aStock sheet to our Company Info
+    yahooEarningsDf_aSymbol_Sheet.to_excel(writer, sheet_name= aStock+'-EarningsHistory',
+                                           startrow=2, startcol= sheetColStart)
+
+    # ------------------------------------------------------------------
+    # setup summary Worksheet
+    # ------------------------------------------------------------------
+    # get 2 rows of summary data
+    summaryRow = yahooEarningsDf_aSymbol_Sheet.iloc[0:1,0:21]
+
+    summaryRow = summaryRow[["Symbol", "Company", "Earnings_Date", "Time", "Close", "ABSFwd1MinusClose", "ABSFwd1PlusClose",
+                             "Exp$Range",  "Volume",  "Option_Volume", "histVol", "impVol", "IV_Delta",
+                              "stdFwd1%", "stdFwd1$TimesClose", "std25Fwd1%",  "std25Fwd1$TimesClose", "max1DayABS$Delta" ]].copy()
+
+    summaryRow.rename(columns={'ABSFwd1MinusClose' : 'ABS $ Minus'}, inplace=True)
+    summaryRow.rename(columns={'ABSFwd1PlusClose' : 'ABS $ Plus'}, inplace=True)
+    summaryRow.rename(columns={'stdFwd1%' : '1-SD % Move'}, inplace=True)
+    summaryRow.rename(columns={"stdFwd1$TimesClose" : '1-SD $ Move'}, inplace=True)
+    summaryRow.rename(columns={'std25Fwd1%' :'2.5-SD % Move'}, inplace=True)
+    summaryRow.rename(columns={'max1DayABS$Delta' :'1Day Max ABS Delta'}, inplace=True)
+    summaryRow.rename(columns={'std25Fwd1$TimesClose' : '2.5-SD $ Move'}, inplace=True)
+    summaryRow.rename(columns={'Earnings_Date': 'Earnings'}, inplace=True)
+    summaryRow.rename(columns={'histVol': 'Historic Vol'}, inplace=True)
+    summaryRow.rename(columns={'impVol': 'Implied Vol'}, inplace=True)
+    summaryRow.rename(columns={'IV_Delta': 'Vol Delta'}, inplace=True)
+
+
+    #  Change to Earning Date Format: "Mon, Jul 06"
+    summaryRow['Earnings'] = summaryRow['Earnings'].apply(str.replace, args=('-', '')).apply(dateUtils.monthDayFormat)
+
+    summaryRow.to_excel(writer, sheet_name= sheetIsFundamentals,
+                        startrow=0, startcol= 1, index=False)
+
+    # ------------------------------------------------------------------
+    # Create Plot and save as png file
+    # ------------------------------------------------------------------
+
+    # ------------------------------------------------------------------
+    # Setup Trade Plan Worksheet
+    # ------------------------------------------------------------------
+    # set up Trade Worksheet as the First Tab this is crudely done by instanciting the sheet first
+    # this is done above   -->  tradePlanWorkbookSheet = writer.sheets[aStock+'-Trade Plan']
+    # Todo - figure out how to order workbook sheets
+    #        the worksheets are kept in "writer" as dict
+    # format line like: 21 Days to expiration on 2020-07-17 -- Implied Volatility: 68.32%
+    expiry_text_format = fundamentalsWorkbook.add_format({'bold': True, 'font_color': '#C00000'})
+    expiry_format = fundamentalsWorkbook.add_format({'bold': True, 'font_color': 'navy', 'border': 2,
+                                                          'valign': 'vcenter', 'align': 'center','fg_color': '#FAF1D3',})
+    today_format = fundamentalsWorkbook.add_format({'bold': True, 'font_color': '#C00000', 'border': 2,
+                                                     'valign': 'vcenter','align': 'center','fg_color': '#FAF1D3',})
+    call_label_format = fundamentalsWorkbook.add_format({'bold': True, 'font_color': 'navy', 'fg_color': '#D7E4BC',
+                                                            'border': 2, 'valign': 'vcenter', 'align': 'center'})
+    put_label_format = fundamentalsWorkbook.add_format({'bold': True, 'font_color': 'navy', 'fg_color': '#f9cfb5',
+                                                                'border': 2, 'valign': 'vcenter', 'align': 'center'})
+    open_format = fundamentalsWorkbook.add_format({'bold': True, 'font_color': 'navy', 'fg_color': '#93D07B',
+                                                            'border': 2, 'valign': 'vcenter', 'align': 'center'})
+    close_format = fundamentalsWorkbook.add_format({'bold': True, 'font_color': 'navy', 'fg_color': '#eb813d',
+                                                            'border': 2, 'valign': 'vcenter', 'align': 'center'})
+    trade_header_format = fundamentalsWorkbook.add_format({'bold': True, 'font_color': 'navy', 'fg_color': '#D7E4BC',
+                                                            'border': 2, 'valign': 'vcenter', 'align': 'center'})
+    trade_text_format = fundamentalsWorkbook.add_format({ 'border': 1, 'valign': 'vcenter', 'align': 'center'})
+    empty_expiryW_format = fundamentalsWorkbook.add_format({'bold': True, 'font_color': 'navy', 'border': 2,
+                                                            'valign': 'vcenter', 'align': 'center','fg_color': '#F2E1A9'})  # f2e1a9
+    empty_expiryM_format = fundamentalsWorkbook.add_format({'border': 2, 'fg_color': '#FAF1D3'})
+    empty_cell_format = fundamentalsWorkbook.add_format({'border': 2, 'valign': 'center'})
+
+    # tradePlanWorkbookSheet = writer.sheets[aStock+'-Trade Plan']
+    tradePlanWorkbookSheet = fundamentalsWorkbook.add_worksheet(aStock + '-Trade Plan')
+
+    [tradePlanWorkbookSheet.write(0, x, '', empty_cell_format) for x in range(0,7,1)]
+
+    for col_num, value in enumerate(summaryRow.columns.values):
+        if col_num > 12:
+            tradePlanWorkbookSheet.write(4, col_num-12, value, trade_header_format)
+            tradePlanWorkbookSheet.write(5, col_num-12, summaryRow.iloc[0,col_num], trade_text_format)
+        elif col_num > 6:
+            tradePlanWorkbookSheet.write(2, col_num-6, value, trade_header_format)
+            tradePlanWorkbookSheet.write(3, col_num-6, summaryRow.iloc[0,col_num], trade_text_format)
+        else:
+            tradePlanWorkbookSheet.write(0, col_num, value, trade_header_format)
+            tradePlanWorkbookSheet.write(1, col_num, summaryRow.iloc[0,col_num], trade_text_format)
+
+    tradePlanWorkbookSheet.set_row(1, 20)
+    tradePlanWorkbookSheet.set_row(3, 20)
+    tradePlanWorkbookSheet.set_row(5, 20)
+
+    # Expiry Text / Implied Vol
+    # next Expiry dates
+    tradePlanWorkbookSheet.write(7,1, 'expiryText', expiry_text_format)
+    tradePlanWorkbookSheet.write(8, 1, "Next Expiry's ->", expiry_format)
+    tradePlanWorkbookSheet.write(8, 2, 'Friday:  ' + dateUtils.getNextFridayExpiryFormat(), empty_expiryW_format)
+    tradePlanWorkbookSheet.write(8, 3, "Monthly:  " + dateUtils.month3Format(dateUtils.getNextExpiryDate()), expiry_format)
+    tradePlanWorkbookSheet.set_column('B:G', 22)
+    # # Save excel
+    writer.save()
+
+
+    # return aWebDriver
+
+def XXXXbuildExcelFile(aStock, startday, theExpiryDateText):
+
+    # Get the company info from BarChart.com // getBarChartData as companyInfo
+    stockInfo = companyInfo.getCompanyStockInfo(aStock)
+    stockOverview = companyInfo.getCompanyOverview(aStock)
+    stockFundamentals = companyInfo.getCompanyFundamentals(aStock)
+    aText, stockRatings = companyInfo.getCompanyRatings(aStock)
     callOptions, putOptions, expiryText, aWebDriver = companyOptions.scrapeCompanyOptionData(aStock, theExpiryDateText)
 
     # Setup Excel output file
@@ -232,7 +398,8 @@ def buildExcelFile(aStock, startday, theExpiryDateText):
     tradePlanWorkbookSheet.set_column('B:G', 22)
     # # Save excel
     writer.save()
-
+    # return the selenium driver -- to control from Jupyter Notebook
+    # not needed to return otherwise
     return aWebDriver
 
 
--- webScrape/getBarChartOptionsSelenium.py
+++ webScrape/getBarChartOptionsSelenium.py
@@ -54,9 +54,62 @@ def downloadCompanyOptions(aStock="AAPL"):
 
     return driver
 
-
-
 def scrapeCompanyOptionData(aStock, theExpiryDateText ):
+
+    # include stock in URL
+    aURL = "http://www.barchart.com/stocks/quotes/" + aStock + "/options"
+
+    # Get Page
+    driver.get(aURL)
+
+    #setup Options page
+    theMoneyness = Select(driver.find_element_by_name('moneyness'))
+    theView = Select(driver.find_element_by_name('view'))
+    theExpiry = Select(driver.find_element_by_xpath('//*[@id="main-content-column"]/div/div[3]/div[1]/div/div[2]/select'))
+
+    # Set Expiration Display
+    #
+    # theView =======================
+    # Stacked           : stacked
+    # Stacked OHLC      : stacked_ohl
+    # Side-by-Side      : sbs
+    # Side-by-Side OHLC : sbs_ohl
+    theView.select_by_value('stacked')
+    # theMoneyness =======================
+    # Near-the-Money  :  inTheMoney
+    # Show All        :  allRows
+    theMoneyness.select_by_value('allRows')
+    theExpiry.select_by_value(theExpiryDateText)
+
+    # get the text to show days to expiry and IV
+    expiryText = driver.find_element_by_xpath("//div[@class='row bc-options-toolbar__second-row']").text
+    expiryText = expiryText.replace('\n', ' -- ')
+
+    optionsTables = read_html(driver.page_source)
+
+    # close the browser
+    # driver.close()
+    # I am now returning driver to the calling program
+    # Because for multiple calls i was getting the invalid session ID error
+    #   That is a WebDriver error that occurs when the server does not
+    #   recognize the unique session identifier.
+    #   this happens if the session has been deleted or if the session ID is invalid.
+
+    callOptions = optionsTables[0]
+    putOptions = optionsTables[1]
+
+    callOptions = callOptions.iloc[:, :-2]
+    putOptions = putOptions.iloc[:, :-2]
+    thePuts = putOptions.set_axis(['Strike', 'Last', '% From Last', 'Bid', 'Midpoint', 'Ask', 'Change',
+                                   '% Chg', 'IV', 'Volume', 'Open Int', 'Time'],axis=1, inplace=False)
+    theCalls = callOptions.set_axis(['Strike', 'Last', '% From Last', 'Bid', 'Midpoint', 'Ask', 'Change',
+                                   '% Chg', 'IV', 'Volume', 'Open Int', 'Time'],axis=1, inplace=False)
+    # callOptions.rename(columns={'Strike', 'Last', '%FromLast', 'Bid', 'Midpoint', 'Ask', 'Change', '%Chg', 'IV', 'Volume', 'OpenInt', 'Time'}, inplace=True)
+
+    return theCalls, thePuts, expiryText, driver
+
+
+def XXXXscrapeCompanyOptionData(aStock, theExpiryDateText ):
 
     # include stock in URL
     aURL = "http://www.barchart.com/stocks/quotes/" + aStock + "/options"
